f276efc41787e98c34e6065f1614eba26f25287e
Voldem999 <vs-kat@mail.ru>
2022-09-22 10:48:06 +0700

    выполнил ДЗ

---
+++ src/Input.java
@@ -0,0 +1,35 @@
+public class Input {
+
+    private static final String VALID_CHARACTERS = "0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+    private static boolean check(String login, String password, String confirmPassword) {
+        try {
+            validate(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+    private static void validate(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login == null || login.length() > 20) {
+            throw new WrongLoginException("Длина логина должна быть меньше или равна 20 символам");
+        }
+        if (password == null || password.length() >= 20) {
+            throw new WrongPasswordException("Длина пароля должна быть меньше 20 символов");
+        }
+        if (confirmPassword == null || confirmPassword.length() >= 20) {
+            throw new WrongPasswordException("Пароли должны совпадать");
+        }
+
+    }
+
+    private static void validateCharacters(boolean isLogin, String s) {
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
+    }
+}
