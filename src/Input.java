ab277fcaaa1b29b93246566c5ce45d55cd93cc53
Voldem999 <vs-kat@mail.ru>
2022-09-22 14:55:37 +0700

    выполнил ДЗ

--- src/Input.java
+++ src/Input.java
@@ -3,7 +3,7 @@ public class Input {
     private static final String VALID_CHARACTERS = "0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
-    private static boolean check(String login, String password, String confirmPassword) {
+    public static boolean check(String login, String password, String confirmPassword) {
         try {
             validate(login, password, confirmPassword);
         } catch (WrongLoginException | WrongPasswordException e) {
@@ -24,11 +24,20 @@ public class Input {
         if (confirmPassword == null || confirmPassword.length() >= 20) {
             throw new WrongPasswordException("Пароли должны совпадать");
         }
+        validateCharacters(true, login);
+        validateCharacters(false, password);
 
     }
 
-    private static void validateCharacters(boolean isLogin, String s) {
+    private static void validateCharacters(boolean isLogin, String s) throws WrongLoginException, WrongPasswordException {
         for (int i = 0; i < s.length(); i++) {
+            if (!VALID_CHARACTERS.contains(String.valueOf(s.charAt(i)))) {
+                if (isLogin) {
+                    throw new WrongLoginException("В логине не верные данные " +s.charAt(i));
+                }else {
+                    throw new WrongPasswordException("В пароле не верные данные " + s.charAt(i));}
+
+            }
             
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        boolean result = Input.check("VS_1993", "123_Kat", "123_Kat");
+        if (result) {
+            System.out.println("Логин и пароль верны");
+        } else {
+            System.out.println("Данные введенны не верно");
+        }
+
     }
 }
